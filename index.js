f605ee330e8e69482472586c17ea0ff8c9b1e094
Alej <pie@Pies-iMac.local>
2019-11-26 07:18:34 +0100

    Add | Another Commit

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const child = require('child_process')
 
 const output = child.execSync(`git log --format=%B%H----`).toString('utf-8')
 
-const commitsArray = output
-  .split('----\n')
-  .map(commit => {
-    const [message, sha] = commit.split('\n')
-
-    return { sha, message }
-  })
-  .filter(commit => Boolean(commit.sha))
+const commitsArray = output.split('----\n').map(v => v)
 
 console.log({ commitsArray })
