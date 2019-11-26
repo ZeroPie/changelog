7403474ffebc93cabf8732ab67cf75e55ad82d10
Alej <pie@Pies-iMac.local>
2019-11-26 07:29:00 +0100

    Change | Use Reduce

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const child = require('child_process')
 
 const output = child.execSync(`git log --format=%B%H----`).toString('utf-8')
 
-const commitsArray = output.split('----\n').map(v => v)
+const commitsArray = output.split('----\n')
 
-console.log({ commitsArray })
+const changelogOb = {
+  adds: [],
+  changes: []
+}
+
+const custob = commitsArray.reduce((acc, ele) => {
+  ele.includes('Add') ? acc.adds.push(ele) : ele
+  ele.includes('Change') ? acc.adds.push(ele) : ele
+
+  return acc
+}, changelogOb)
+/*   .map(commit => {
+    const [message, sha] = commit.split('\n')
+
+    return { sha, message }
+  })
+  .filter(commit => Boolean(commit.sha))
+ */
+
+console.log(custob)
